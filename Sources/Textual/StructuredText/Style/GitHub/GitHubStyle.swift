95584f64eab938f333387d51e8fe03cdf009ef79
Peter Bray <petertheyeti@gmail.com>
2026-06-07 10:40:45 -0700

    Revert "feat(style): make GitHubStyle code block style injectable"
    This reverts commit 8385359664dbcaaedab7fae9fcfb3d21c6436b22.

--- Sources/Textual/StructuredText/Style/GitHub/GitHubStyle.swift
+++ Sources/Textual/StructuredText/Style/GitHub/GitHubStyle.swift
@@ -3,15 +3,13 @@ import SwiftUI
 extension StructuredText {
   /// A GitHub-like set of styles for structured text.
   ///
-  /// Apply with ``TextualNamespace/structuredTextStyle(_:)``. Pass a custom
-  /// ``StructuredText/CodeBlockStyle`` to replace the default GitHub code block
-  /// chrome while keeping every other block style unchanged.
-  public struct GitHubStyle<CodeBlock: StructuredText.CodeBlockStyle>: Style {
+  /// Apply this style with ``TextualNamespace/structuredTextStyle(_:)``.
+  public struct GitHubStyle: Style {
     public let inlineStyle: InlineStyle = .gitHub
     public let headingStyle: GitHubHeadingStyle = .gitHub
     public let paragraphStyle: GitHubParagraphStyle = .gitHub
     public let blockQuoteStyle: GitHubBlockQuoteStyle = .gitHub
-    public let codeBlockStyle: CodeBlock
+    public let codeBlockStyle: GitHubCodeBlockStyle = .gitHub
     public let listItemStyle: DefaultListItemStyle = .default
     public let unorderedListMarker: HierarchicalSymbolListMarker = .hierarchical(
       .disc, .circle, .square)
@@ -20,21 +18,11 @@ extension StructuredText {
     public let tableCellStyle: GitHubTableCellStyle = .gitHub
     public let thematicBreakStyle: GitHubThematicBreakStyle = .gitHub
 
-    public init(codeBlockStyle: CodeBlock) {
-      self.codeBlockStyle = codeBlockStyle
-    }
+    public init() {}
   }
 }
 
-extension StructuredText.GitHubStyle where CodeBlock == StructuredText.GitHubCodeBlockStyle {
-  /// Creates the default GitHub style (stock code block chrome).
-  public init() {
-    self.init(codeBlockStyle: .gitHub)
-  }
-}
-
-extension StructuredText.Style
-where Self == StructuredText.GitHubStyle<StructuredText.GitHubCodeBlockStyle> {
+extension StructuredText.Style where Self == StructuredText.GitHubStyle {
   /// A GitHub-like structured text style.
   public static var gitHub: Self {
     .init()
